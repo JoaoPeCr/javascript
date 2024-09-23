1f18f59cecce316b6d4d4faca1076bcc4b791973
João P. Corrêa <154474733+JoaoPeCr@users.noreply.github.com>
2024-09-23 20:06:14 -0300

    ,

--- modulos/aula12ex/modelo copy/script.js
+++ modulos/aula12ex/modelo copy/script.js
@@ -2,29 +2,26 @@ function carregar() {
   var msg = window.document.getElementById('msg')
   var img = window.document.getElementById('imagem')
   var data = new Date()
-  //var horaAtual = data.getHours()
-  var hora = 22
-  msg.innerHTML = `Agora são ${horaAtual} hs`
-  /*if (hora > 24) {
+  var hora = data.getHours()
+  msg.innerHTML = `Agora são ${hora} hs`
+  if (hora > 24) {
     //erro
+    document.body.style.background = '#cc8000'
   } else if (hora < 6) {
     //madrugada 
-    img.src = 'pexels-george-desipris-858241.jpg'
+    img.src = 'pexels-sanaan-3052361.jpg'
+    document.body.style.background = '#192928'
   } else if (hora <= 12) {
     //manhã
     img.src = 'pexels-artur-roman-158558-1167355.jpg'
+    document.body.style.background = '#D5D3B8'
   } else if (hora <= 18) {
     //tarde
     img.src = 'pexels-george-desipris-858241.jpg'
+    document.body.style.background = '#7D5037'
   } else {
     //noite
-    img.src = 'pexels-george-desipris-858241.jpg'
-  } */
- if (hora >= 0 && hora < 12) {
-  img.src = 'teste.png'
- } else if (hora >= 12 && hora < 18) {
-  img.src = 'teste.png'
- } else {
-  img.src = 'teste.png'
- }
+    img.src = 'pexels-sanaan-3052361.jpg'
+    document.body.style.background = '#192928'
+  }
 } 
